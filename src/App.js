05fc3118b556c3267bf52b240125b7aa877369c2
Weidong Li <leonlidawn@gmail.com>
2019-09-22 02:02:20 +1000

    outlined layouts

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="cardHeader">
+
+        <div className="currentWeather">
+
+          <div className="basicDetails">
+            <div className="temperature">12</div>
+            <div className="status">cloudy</div>
+          </div>
+
+          <div className="additionalDetails">
+            <div className="humidity">
+              <div>HUMIDITY</div>
+              <div>64</div>
+
+            </div>
+            <div className="wind">
+              <div>WIND</div>
+              <div>12</div>
+            </div>
+          </div>
+
+        </div>
+        <div className="gap"></div>
+        <div className="country">france</div>
+
+      </div>
+
+      <div className="cardBody">
+
+        <div className="twitterFeedSection">
+          <div className="header">
+            <div className="title">Twitter Feed</div>
+            <div className="country">france</div>
+          </div>
+          <div className="body">
+            <div className="tweets">
+              <div className="tweet">tweet content </div>
+              <div className="tweet">tweet content </div>
+            </div>
+            <button className="navigation">Next</button>
+          </div>
+        </div>
+
+        <div className="weekDayDetails">
+          <div className="weekDay">mon</div>
+          <div className="weekDay">tue</div>
+          <div className="weekDay">wed</div>
+          <div className="weekDay">thu</div>
+          <div className="weekDay">fri</div>
+        </div>
+
+
+      </div>
     </div>
   );
 }
